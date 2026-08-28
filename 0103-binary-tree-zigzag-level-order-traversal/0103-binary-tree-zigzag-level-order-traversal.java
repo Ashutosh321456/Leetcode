f51b80b87a6b3b089987f782ef4839993d91809c
Ashutosh321456 <kumarashutosh96587@gmail.com>
2026-08-28 23:12:03 +0530

    Time: 1 ms (81.19%), Space: 43.8 MB (25.28%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Pair{
+    TreeNode node;
+    int level;
+    Pair(TreeNode node , int level){
+        this.node = node;
+        this.level = level;
+    }
+}
+class Solution {
+     private static void levelOrderLineWise(TreeNode root , List<List<Integer>> ans) {
+        if (root == null) return;
+        
+        Queue<Pair> q = new LinkedList<>();
+        int currLevel = 0;
+        q.add(new Pair(root,0));
+
+        List<Integer> subList = new ArrayList<>();
+
+        while(q.size()>0){
+            Pair front = q.remove();
+            TreeNode node = front.node;
+            int level = front.level;
+
+            if(level!=currLevel){
+                ans.add(subList);
+                subList = new ArrayList<>();
+                currLevel = level;
+            }
+            if(level%2!=0) subList.add(0 , node.val);
+            else subList.add(node.val);
+            
+                if(node.left!=null) q.add(new Pair(node.left,level+1));
+                if(node.right!=null) q.add(new Pair(node.right,level+1));
+        }
+        if (!subList.isEmpty()) {
+            ans.add(subList);
+        }
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        levelOrderLineWise(root, ans);
+        return ans;
+    }
+}
